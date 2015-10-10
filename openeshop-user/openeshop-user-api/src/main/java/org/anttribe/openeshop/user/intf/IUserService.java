47a5e67139e20ee130a12fa8252232d8bf0e291a
anshenglimin@163.com <anshenglimin@163.com>
2015-10-10 18:01:56 +0800

    initial project openeshop-user

---
+++ openeshop-user/openeshop-user-api/src/main/java/org/anttribe/openeshop/user/intf/IUserService.java
@@ -0,0 +1,16 @@
+/*
+ * 文  件   名: IUserService.java
+ * 版         本 : (Anttribe) openeshop-user-api All rights reserved
+ * 描         述 : <描述>
+ * 修   改  人: zhaoyong
+ * 修改时间: 2015年10月10日
+ */
+package org.anttribe.openeshop.user.intf;
+
+/**
+ * @author zhaoyong
+ * @version 2015年10月10日
+ */
+public interface IUserService
+{
+}
